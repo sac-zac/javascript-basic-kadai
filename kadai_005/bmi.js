ce88c6ffe27a58b1eeff97c0e41694ae29e2b1c0
sac-zac <bankoucaizi1@gmail.com>
2025-04-19 10:05:55 +0900

    js基本課題1
    js基本課題 v1

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+// 変数の宣言・値の代入
+let userWeight = 68;  //体重(kg)
+let userHeight = 1.7;  //身長(m)
+let userBmi;  //BMI値
+
+// BMI演算
+userBmi = userWeight / (userHeight * userHeight)
+
+// BMI演算結果出力
+console.log(userBmi);

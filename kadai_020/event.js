674d469c6821a242012c8e7e29679a990cdc2c04
sac-zac <bankoucaizi1@gmail.com>
2025-05-11 13:59:38 +0900

    Delete kadai_020 directory
    再々提出

--- kadai_020/event.js
+++
@@ -1,9 +0,0 @@
-// HTML要素がクリックされたときにイベント処理を実行する
-// 「ボタンをクリックしました」に変更
-const btn = document.getElementById('btn');
-const txt = document.getElementById('text');
-
-btn.addEventListener('click', () => {
-  txt = document.getElementById('text').textContent = 'ボタンをクリックしました';
-  console.log(txt);
-});

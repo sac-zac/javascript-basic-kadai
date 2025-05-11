60fd700b298ce8ed6a8b326191bbe44582474cbc
sac-zac <bankoucaizi1@gmail.com>
2025-05-11 14:01:26 +0900

    Delete kadai_021 directory
    再提出

--- kadai_021/event.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-btn.addEventListener('click', () => {
-    setTimeout(() => {
-    text = document.getElementById('text').textContent = 'ボタンをクリックしました';
-    console.log(text);
-    }, 2000);
-});
